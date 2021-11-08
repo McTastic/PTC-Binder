727e6aecb0df80764e60f645147ee03602d607d6
McTastic <mctastic2121@gmail.com>
2021-11-08 17:50:48 -0500

    added dashboard route to index of api folder

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homePage = require("./homepage");
+const dashboardRoutes = require("./dashboardRoutes");
 
 router.use("/", homePage);
 router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 router.get("*", async (req, res) => {
   try {
