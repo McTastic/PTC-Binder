769ea4f1728c25c7f84222c59ba1051c33a3a878
McTastic <mctastic2121@gmail.com>
2021-11-11 19:14:45 -0500

    commented out un-needed code in dashboard route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
-const withAuth = require("../utils/auth");
-const { User, Binder } = require("../models");
+// const withAuth = require("../utils/auth");
+// const { User, Binder } = require("../models");
 
 // Use withAuth middleware to prevent access to route
 router.get("/", async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Binder }],
-    });
+    // const userData = await User.findByPk(req.session.user_id, {
+    //   attributes: { exclude: ["password"] },
+    //   include: [{ model: Binder }],
+    // });
 
-    const user = userData.get({ plain: true });
+    // const user = userData.get({ plain: true });
     console.log("**************************", user);
     res.render("dashboard", {
       ...user,
