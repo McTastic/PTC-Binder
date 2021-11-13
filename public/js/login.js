41d0c13483bc4269ebd3fa7f7e7cf5aea7227272
McTastic <mctastic2121@gmail.com>
2021-11-12 21:31:56 -0500

    fixed login html IDs so the listener can work

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,8 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
   console.log("Calling login...");
 
-  const usernameEl = document.querySelector("#username").value.trim();
-  const passwordEl = document.querySelector("#password").value.trim();
+  const usernameEl = document.querySelector("#username-login").value.trim();
+  const passwordEl = document.querySelector("#password-login").value.trim();
 
   if (usernameEl && passwordEl) {
     const response = await fetch("/api/users/login", {
@@ -23,9 +23,9 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector("#username").value.trim();
-  const email = document.querySelector("#email").value.trim();
-  const password = document.querySelector("#password").value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
   if (username && email && password) {
     const response = await fetch("/api/users", {
