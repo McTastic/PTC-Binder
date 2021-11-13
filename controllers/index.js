ddaa18fda8d92f84b776724482b4628fca592885
McTastic <mctastic2121@gmail.com>
2021-11-12 21:45:33 -0500

    connected binder route

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,13 @@ const apiRoutes = require("./api");
 const dashboardRoutes = require("./dashboardRoutes");
 const loginRoutes = require("./loginRoutes");
 const searchRoutes = require("./searchResultRoute");
+const binderRoutes = require("./binderRoutes");
 
 router.use("/", dashboardRoutes);
 router.use("/api", apiRoutes);
 router.use("/login", loginRoutes);
 router.use("/search", searchRoutes);
+router.use("/binder", binderRoutes);
 // router.use("/dashboard", dashboardRoutes);
 
 router.get("*", async (req, res) => {
