b483aeb295bc9e5253848d4427d008daf945ca94
Steven Sosebee <swsosebee@gmail.com>
2021-11-16 10:56:49 -0500

    removing unnecessary npm package refs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const binderRoutes = require("./binderRoutes");
 const inventoryRoutes = require("./InventoryRoutes");
-const ptcRoutes = require("./PTCcall");
-const cartRoutes = require("./cartRoutes");
+// const ptcRoutes = require("./PTCcall");
+// const cartRoutes = require("./cartRoutes");
 
 router.use("/users", userRoutes);
 router.use("/binder", binderRoutes);
 router.use("/inventory", inventoryRoutes);
-router.use("/data", ptcRoutes);
-router.use("/cart", cartRoutes);
+// router.use("/data", ptcRoutes);
+// router.use("/cart", cartRoutes);
 module.exports = router;
